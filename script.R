2858a538c44674742fc7e6a86f204b700df51258
Jensen Joymangul <jensen.joymangul@etu.univ-lyon1.fr>
2018-01-01 17:43:24 +0100

    Select only the relevant variables

--- script.R
+++ script.R
@@ -9,7 +9,7 @@ df_obs <- read.table("FW_groupe8_obs.txt", header = TRUE)
 
 ###################################################################
 #             Linear regression variable selection 
-#                 regresion by 1 varia
+#                 regresion by 1 variable
 ###################################################################
 # Get number of descriptor
 n_descriptor <- ncol(df)
@@ -34,8 +34,80 @@ corr_desciptors <- corr_desciptors[1:(corr_desciptors_counter),]
 
 # Remove on descriptor which are too correlated
 corr_desciptors <- unique(c(corr_desciptors))
-new <- subset(df, select = c(corr_desciptors)) 
+new_df <- df[ , -which(names(df) %in% c(corr_desciptors))]
 
-for (i in 1:corr_desciptors_counter) {
-  print(corr_desciptors[i,2])
+###################################################################
+#             Linear regression variable selection 
+#                 regresion by 2 variables
+###################################################################
+# Get number of descriptor
+n_descriptor <- ncol(new_df)
+#Matrix to store index of correlate descriptor
+corr_desciptors_1 <- matrix(nrow=1500)
+corr_desciptors_2 <- matrix(nrow=1500)
+# Counter for number of correlates descriptor
+corr_desciptors_counter <- 0
+for (i in 1:n_descriptor) {
+  for (j in i:n_descriptor) {
+    if(i != j) {
+      for (k in j:n_descriptor) {
+        if (j != k) {
+          reg0 = lm(df[,i] ~ df[,j] + df[,k], data=new_df)
+          r2 <- summary(reg0)$r.squared
+          if (r2 > 0.90) {
+            corr_desciptors_counter <- corr_desciptors_counter + 1
+            corr_desciptors_1[corr_desciptors_counter, 1]= colnames(new_df)[i]
+            corr_desciptors_2[corr_desciptors_counter, 1]= colnames(new_df)[k]
+          }
+        }
+      }
+    }
+  }
 }
+
+# Truncate the corr_descriptors matrix
+corr_desciptors_1 <- corr_desciptors_1[1:(corr_desciptors_counter),]
+
+# Remove on descriptor which are too correlated
+corr_desciptors_1 <- unique(c(corr_desciptors_1))
+new_df <- new_df[ , -which(names(new_df) %in% c(corr_desciptors_1))]
+
+###################################################################
+#             Linear regression variable selection 
+#                 regresion by 3 variables
+###################################################################
+# Get number of descriptor
+n_descriptor <- ncol(new_df)
+#Matrix to store index of correlate descriptor
+corr_desciptors_1 <- matrix(nrow=4000)
+corr_desciptors_2 <- matrix(nrow=4000)
+# Counter for number of correlates descriptor
+corr_desciptors_counter <- 0
+for (i in 1:n_descriptor) {
+  for (j in i:n_descriptor) {
+    if(i != j) {
+      for (k in j:n_descriptor) {
+        if (j != k) {
+          for (l in j:n_descriptor) {
+            if (k != l ) {
+              reg0 = lm(df[,i] ~ df[,j] + df[,k] + df[,l], data=new_df)
+              r2 <- summary(reg0)$r.squared
+              if (r2 > 0.92) {
+                corr_desciptors_counter <- corr_desciptors_counter + 1
+                corr_desciptors_1[corr_desciptors_counter, 1]= colnames(new_df)[i]
+                corr_desciptors_2[corr_desciptors_counter, 1]= colnames(new_df)[k]
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# Truncate the corr_descriptors matrix
+corr_desciptors_1 <- corr_desciptors_1[1:(corr_desciptors_counter),]
+
+# Remove on descriptor which are too correlated
+corr_desciptors_1 <- unique(c(corr_desciptors_1))
+new_df <- new_df[ , -which(names(new_df) %in% c(corr_desciptors_1))]
