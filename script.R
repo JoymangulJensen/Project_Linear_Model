a8131d181ba144257c1001155f8d0d73968cbe14
Jensen Joymangul <jensen.joymangul@etu.univ-lyon1.fr>
2018-01-03 19:33:51 +0100

    Implement regressions

--- script.R
+++ script.R
@@ -35,6 +35,7 @@ corr_desciptors <- corr_desciptors[1:(corr_desciptors_counter),]
 # Remove on descriptor which are too correlated
 corr_desciptors <- unique(c(corr_desciptors))
 new_df <- df[ , -which(names(df) %in% c(corr_desciptors))]
+new_df_obs <- df_obs[ , -which(names(df_obs) %in% c(corr_desciptors))]
 
 ###################################################################
 #             Linear regression variable selection 
@@ -52,7 +53,7 @@ for (i in 1:n_descriptor) {
     if(i != j) {
       for (k in j:n_descriptor) {
         if (j != k) {
-          reg0 = lm(df[,i] ~ df[,j] + df[,k], data=new_df)
+          reg0 = lm(new_df[,i] ~ new_df[,j] + new_df[,k], data=new_df)
           r2 <- summary(reg0)$r.squared
           if (r2 > 0.95) {
             corr_desciptors_counter <- corr_desciptors_counter + 1
@@ -71,6 +72,7 @@ corr_desciptors_1 <- corr_desciptors_1[1:(corr_desciptors_counter),]
 # Remove on descriptor which are too correlated
 corr_desciptors_1 <- unique(c(corr_desciptors_1))
 new_df <- new_df[ , -which(names(new_df) %in% c(corr_desciptors_1))]
+new_df_obs <- new_df_obs[ , -which(names(new_df_obs) %in% c(corr_desciptors_1))]
 
 ###################################################################
 #             Linear regression variable selection 
@@ -90,7 +92,7 @@ for (i in 1:n_descriptor) {
         if (j != k) {
           for (l in j:n_descriptor) {
             if (k != l ) {
-              reg0 = lm(df[,i] ~ df[,j] + df[,k] + df[,l], data=new_df)
+              reg0 = lm(new_df[,i] ~ new_df[,j] + new_df[,k] + new_df[,l], data=new_df)
               r2 <- summary(reg0)$r.squared
               if (r2 > 0.95) {
                 corr_desciptors_counter <- corr_desciptors_counter + 1
@@ -111,3 +113,61 @@ corr_desciptors_1 <- corr_desciptors_1[1:(corr_desciptors_counter),]
 # Remove on descriptor which are too correlated
 corr_desciptors_1 <- unique(c(corr_desciptors_1))
 new_df <- new_df[ , -which(names(new_df) %in% c(corr_desciptors_1))]
+new_df_obs <- new_df_obs[ , -which(names(new_df_obs) %in% c(corr_desciptors_1))]
+
+###################################################################
+#                     Regression  Stepwise
+###################################################################
+# Get number of descriptor
+n_descriptor_obs <- ncol(new_df_obs)
+
+# Init the R2
+best_r2 <- 0
+
+for (i in 2:n_descriptor_obs) {
+  for (j in i:n_descriptor_obs) {
+    if(i != j) {
+      for (k in j:n_descriptor_obs) {
+        if (j != k) {
+          for (l in j:n_descriptor_obs) {
+            if (k != l ) {
+              reg0 = lm(new_df_obs[,1] ~ new_df_obs[,i] + new_df_obs[,j] + new_df_obs[,k] + new_df_obs[,l], data=new_df_obs)
+              r2 <- summary(reg0)$r.squared
+              if (r2 > best_r2) {PRESS(mod)
+
+                best_r2 <- r2
+                best_variables_1 <- colnames(new_df_obs)[i]
+                best_variables_2 <- colnames(new_df_obs)[j]
+                best_variables_3 <- colnames(new_df_obs)[k]
+                best_reg <- reg0
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+###################################################################
+#                     Calculate the PRESS
+###################################################################
+sum(best_reg$residuals^2)
+
+###################################################################
+#           Regression  ‘Least Absolute Deviation'
+###################################################################
+install.packages("Blossom")
+library(Blossom)
+obs <- new_df_obs[,1]
+descriptor_only <- new_df_obs[-c(1)]
+reponse_str <- "reponse"
+names <-  colnames(descriptor_only)
+formula <-paste(reponse_str, paste(names, collapse=" + "), sep=" ~ ")
+
+Out <- lad(formula,data = new_df_obs,test = TRUE)
+
+summary(Out)
+residuals(Out)
+predict(Out)
+coefficients(Out)
